301ac2594668a6a2f2ad7bdc322127404dc14f62
evandritter <78986291+evandritter@users.noreply.github.com>
2021-04-15 13:16:25 -0500

    Add files via upload

---
+++ Collision/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Collision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject blueGhost = new GameObject(42, 42);
+            GameObject yellowGhost = new GameObject(10, 25);
+            blueGhost.Collision(yellowGhost);
+            Console.WriteLine(Convert.ToString((blueGhost.velocityX, blueGhost.velocityY, 
+            yellowGhost.velocityX, yellowGhost.velocityY)));
+        }
+    }
+
+    public class GameObject
+    {
+
+        public float velocityX;
+        public float velocityY;
+        public GameObject(float velocityX, float velocityY)
+        {
+            this.velocityX = velocityX;
+            this.velocityY = velocityY;
+        }
+
+        public virtual void Collision(GameObject HitObj)
+        {
+            float temp;
+
+            //swapping the values i.e.collision
+            temp = this.velocityX;
+            this.velocityX = HitObj.velocityX;
+            HitObj.velocityX = temp;
+
+            temp = this.velocityY;
+            this.velocityY = HitObj.velocityY;
+            HitObj.velocityY = temp;
+        }
+
+    }
+
+    public class Ghost : GameObject
+    {
+        public Ghost(float velocityX, float velocityY) : base(velocityX, velocityY)
+        { }
+        public override void Collision(GameObject HitObj)
+        {   //only ghosts collide
+            if (HitObj is Ghost)
+            {
+                base.Collision(HitObj);
+            }
+
+        }
+    }
+
+
+
+}
